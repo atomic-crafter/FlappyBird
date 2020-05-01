b0a12daac019fcb72983f6aaadd843244ada3f1d
atomic-crafter <antonin.rossin04@gmail.com>
2020-05-01 16:37:30 +0200

    Update FlappyBird.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -14,7 +14,7 @@ def draw_window(screen):
 	screen.blit(BACKGROUND, (0,0))
 	pygame.display.update()
 
-def principal():
+def main():
 	run = True
 	screen = pygame.display.set_mode((longeur,largeur))
 	clock = pygame.time.Clock()
@@ -54,4 +54,28 @@ class Bird:
 
 
 	def jump(self):
+		self.velocity = -10.5
+		self.tick_count = 0
+		self.height = self.y
+
+
+	def move(self):
+		self.tick_count = self.tick_count + 1
+		d = self.velocity * self.tick_count + 0.5*3*self.tick_count**2
+
+		if (d >= 16):
+			d = ((d / abs(d))*16)
+
+		if (d<= 0):
+			d = d - 2
+		self.y = self.y + d
+		if (d < 0 or self.y < self.height + 50):
+			if (self.tilt < self.MAX_ROTATION):
+				self.tilt = self.MAX_ROTATION
+		else:
+			if (self.tilt > 90):
+				self.tilt = self.tilt - self.ROT_VEL
+
+
+	def draw(self,screen):
 		
