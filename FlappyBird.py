0bfb1a6449f73c7e01f00109ee86a806628a32d8
atomic-crafter <antonin.rossin04@gmail.com>
2020-05-01 16:47:48 +0200

    Update FlappyBird.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -78,4 +78,20 @@ class Bird:
 
 
 	def draw(self,screen):
-		
+		self.img_count += 1
+
+		if (self.img_count < self.ANIMATION_TIME):
+			self.img = self.IMGS[0]
+		elif (self.img_count < self.ANIMATION_TIME*2):
+			self.img = self.IMGS[1]
+		elif (self.img_count < self.ANIMATION_TIME*3):
+			self.img = self.IMGS[2]
+		elif (self.img_count < self.ANIMATION_TIME*4):
+			self.img = self.IMGS[1]
+		elif (self.img_count == self.ANIMATION_TIME*4 + 1):
+			self.img = self.IMGS[0]
+			self.img_count = 0
+
+		if (self.tilt <= -80):
+			self.img = self.IMGS[1]
+			self.img_count = self.ANIMATION_TIME * 2
